ee7de8913dcf256b6284676ac6d245ac877f0a00
victorpawlek <pawlek.v03@htlwienwest.at>
2022-03-22 08:22:04 +0100

    start

---
+++ client/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <p class="text-h3 text-center mt-4"><v-icon class="text-h3 red--text">mdi-heart</v-icon> Willkommen! <v-icon class="text-h3 red--text">mdi-heart</v-icon></p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+};
+</script>
